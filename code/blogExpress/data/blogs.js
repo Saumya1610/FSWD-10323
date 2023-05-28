3a3c3bda1f31caca277f8d81e55706f96033cf12
Saumya <saumyaagrawal106@gmail.com>
2023-05-28 23:10:59 +0530

    demo for ExpressJS

---
+++ code/blogExpress/data/blogs.js
@@ -0,0 +1,23 @@
+blogs =  [
+        {
+            title: "How to get started with ExpressJS",
+            content: "this is content",
+            slug:"express-learn"
+        },
+        {
+            title: "How to get started with Python",
+            content: "this is content",
+            slug:"python-learn"
+        },
+        {
+            title: "How to get started with CSS",
+            content: "this is content",
+            slug:"css-learn"
+        },
+        {
+            title: "How to get started with HTML",
+            content: "this is content",
+            slug:"html-learn"
+        }
+    ]
+module.exports = blogs;
